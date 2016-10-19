82d6b78b24d6173fd1099e14f8a4e7310a582da6
umoruj <umoru80@gmail.com>
2016-10-19 16:22:16 +0200

    first commit

--- Audio2/ViewController.swift
+++ Audio2/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "sample", ofType: "m4a")!))
+            audioPlayer.prepareToPlay()
+            
+            let audioSession = AVAudioSession.sharedInstance()
+            
+            do{
+                try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+            }
+            catch{
+                
+            }
+        
+        }
+        catch{
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func play(_ sender: AnyObject) {
+        audioPlayer.play()
+    }
 
+    @IBAction func pause(_ sender: AnyObject) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        }else{
+            
+        }
+    }
+    @IBAction func restart(_ sender: AnyObject) {
+        if audioPlayer.isPlaying {
+            audioPlayer.currentTime = 0
+            audioPlayer.play()
+        }else{
+            audioPlayer.play()
+        }
+    }
 }
 
